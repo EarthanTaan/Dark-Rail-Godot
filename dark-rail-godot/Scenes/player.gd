acce593c596605404da4ec92361a487ec88d89ec
Earthan Huq <earthan@gmail.com>
2025-04-21 10:56:04 -0400

    Cleaning up and commenting code.
    Next step is to recreate my train controls. I am not entirely sure the GUIDE plugin is making my work more efficient.

--- dark-rail-godot/Scenes/player.gd
+++ dark-rail-godot/Scenes/player.gd
@@ -8,12 +8,20 @@ const JUMP_VELOCITY = 4.5
 
 func _physics_process(delta: float) -> void:
 	velocity = basis * move.value_axis_3d * SPEED
+	
+	# Use mouse to look around, escape key toggles cursor visibility.
+	mouse_look()
+	
+	# I don't really know what this does it's one of Godot's built-in functions.
+	move_and_slide()
+
+func mouse_look():
+	# Mouse's Y position relative to screen size is scaled 360 degrees:
 	rotation_degrees.y += look.value_axis_2d.x
+	# Mouse's X position relative to screen size is scaled to 180 degrees:
 	camera.rotation_degrees.x += look.value_axis_2d.y
+	# Stop looking up or down when you get to straight up or straight down:
 	if camera.rotation_degrees.x > 90:
 		camera.rotation_degrees.x = 90
 	if camera.rotation_degrees.x < -90:
 		camera.rotation_degrees.x = -90
-	
-	move_and_slide()
-	
