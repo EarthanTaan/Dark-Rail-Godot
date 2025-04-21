1fdd2f3b10203b6e8c5be66c7687779deae9e5b5
Earthan Huq <earthan@gmail.com>
2025-04-21 10:45:21 -0400

    Finally got walking movement controls
    Including using the escape key to toggle mouse capture on and off, and mouse-looking, using a probably-inefficient combination of GUIDE and Godot Input System.

--- dark-rail-godot/Scenes/main.gd
+++ dark-rail-godot/Scenes/main.gd
@@ -1,8 +1,22 @@
 extends Node3D
 
 @export var walk_mode:GUIDEMappingContext
-@export var free_mouse:GUIDEAction
+var mouse_captured = true
 
-func _ready() -> void:
+func _ready():
 	GUIDE.enable_mapping_context(walk_mode)
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event):
+	if event.is_action_pressed("escape_mouse"):
+		cursor_toggle()
+
+func cursor_toggle():
+	match mouse_captured:
+		true:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		false:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	if Input.is_action_just_pressed("escape_mouse"):
+		mouse_captured = !mouse_captured
--- dark-rail-godot/Scenes/player.gd
+++ dark-rail-godot/Scenes/player.gd
@@ -1,18 +1,19 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 @onready var camera = $player_camera
-@export var free_mouse:GUIDEAction
 @export var move:GUIDEAction
 @export var look:GUIDEAction
 
-
 func _physics_process(delta: float) -> void:
 	velocity = basis * move.value_axis_3d * SPEED
-	rotation_degrees.y += look.value_axis_1d
-	
-	
+	rotation_degrees.y += look.value_axis_2d.x
+	camera.rotation_degrees.x += look.value_axis_2d.y
+	if camera.rotation_degrees.x > 90:
+		camera.rotation_degrees.x = 90
+	if camera.rotation_degrees.x < -90:
+		camera.rotation_degrees.x = -90
 	
 	move_and_slide()
+	
