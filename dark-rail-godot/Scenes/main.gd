0d99ab5bd30421aa96757fe8ce8dfc8779bcdb20
Earthan Huq <earthan@gmail.com>
2025-04-21 17:22:21 -0400

    The GUIDE system may be holding me back
    I think the throttle lever system was probably easier to code directly into GDScript... I'm gonna scream. I don't have time for this.

--- dark-rail-godot/Scenes/main.gd
+++ dark-rail-godot/Scenes/main.gd
@@ -1,22 +1,50 @@
 extends Node3D
-
+# Introduce the GUIDE mapping contexts.
 @export var walk_mode:GUIDEMappingContext
-var mouse_captured = true
+@export var shared_context:GUIDEMappingContext
+@export var pilot_mode:GUIDEMappingContext
+
+# Start with the mouse captured by default.
+@onready var mouse_captured = true
+
+# "T" key toggles control modes between walk and pilot mapping contexts.
+@export var mode_swap:GUIDEAction
+# Context mode Toggle begins in off position.
+@onready var pilot_mode_active = false
 
 func _ready():
 	GUIDE.enable_mapping_context(walk_mode)
+	GUIDE.enable_mapping_context(shared_context)
+	
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
+# Input activates only when there is an input to read (?)
 func _input(event):
 	if event.is_action_pressed("escape_mouse"):
 		cursor_toggle()
 
+func _process(delta):
+	if mode_swap.is_triggered():
+		swap_modes()
+
 func cursor_toggle():
+	if Input.is_action_just_pressed("escape_mouse"):
+		mouse_captured = !mouse_captured
 	match mouse_captured:
 		true:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 		false:
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+func swap_modes():
+	pilot_mode_active = !pilot_mode_active
+	match pilot_mode_active :
+		true:
+			GUIDE.enable_mapping_context(pilot_mode)
+			GUIDE.disable_mapping_context(walk_mode)
+		false:
+			GUIDE.enable_mapping_context(walk_mode)
+			GUIDE.disable_mapping_context(pilot_mode)
+			
+
 	
-	if Input.is_action_just_pressed("escape_mouse"):
-		mouse_captured = !mouse_captured
--- dark-rail-godot/Scenes/player.gd
+++ dark-rail-godot/Scenes/player.gd
@@ -3,25 +3,57 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 @onready var camera = $player_camera
+@onready var track_switch_lever = false # false = left / true = right
+
+@export var pilot_mode:GUIDEMappingContext
 @export var move:GUIDEAction
 @export var look:GUIDEAction
+@export var train_throttle:GUIDEAction
+@export var train_ebrake:GUIDEAction
+@export var train_switch_toggle:GUIDEAction
 
-func _physics_process(delta: float) -> void:
+var throttle_setting:int
+
+func _physics_process(delta):
+	# Move with WASD
+	# 'velocity' is built-in body-motion from move_and_slide(); 'basis' is built-in for relative rotation;
+	# 'move' creates direction (x, x, 0) relative to basis; multiply by SPEED to move
 	velocity = basis * move.value_axis_3d * SPEED
 	
-	# Use mouse to look around, escape key toggles cursor visibility.
-	mouse_look()
+	# Call my functions.
+	mouse_look() # Look with the mouse.
+	control_train() # Control the train when train-context is enabled.
 	
 	# I don't really know what this does it's one of Godot's built-in functions.
 	move_and_slide()
 
+
+# Use mouse to look around.
 func mouse_look():
-	# Mouse's Y position relative to screen size is scaled 360 degrees:
+	# Mouse's Y position relative to screen size is scaled to 360 (for degrees):
 	rotation_degrees.y += look.value_axis_2d.x
-	# Mouse's X position relative to screen size is scaled to 180 degrees:
+	# Mouse's X position relative to screen size is scaled to 180 (for degrees):
 	camera.rotation_degrees.x += look.value_axis_2d.y
-	# Stop looking up or down when you get to straight up or straight down:
+	# Prevent vertical look past straight up or down:
 	if camera.rotation_degrees.x > 90:
 		camera.rotation_degrees.x = 90
 	if camera.rotation_degrees.x < -90:
 		camera.rotation_degrees.x = -90
+
+func control_train():
+	# train_throttle
+	if GUIDE.is_mapping_context_enabled(pilot_mode):
+		# This is basically working, it makes a number go up and down. Now I just have to refine the effect.
+		# Once again I am asking myself if this is better than just coding it in Godot's Input System.
+		# I'm gonna scream. Why is it continually inputing?
+		throttle_setting += train_throttle.value_axis_1d
+		print(throttle_setting)
+		# train_ebrake
+		# train_switch_toggle
+		if train_switch_toggle.is_triggered():
+			track_switch_lever = !track_switch_lever
+			match track_switch_lever:
+				false: print("Track Switch: Left")
+				true: print("Track Switch: Right")
+	
+	
