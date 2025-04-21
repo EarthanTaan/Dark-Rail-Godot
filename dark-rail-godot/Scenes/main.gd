69fcf5bbcfa0edec82a79588802c923384dcf436
Earthan Huq <earthan@gmail.com>
2025-04-21 08:12:27 -0400

    Still configuring controls

--- dark-rail-godot/Scenes/main.gd
+++ dark-rail-godot/Scenes/main.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
 @export var walk_mode:GUIDEMappingContext
+@export var free_mouse:GUIDEAction
 
 func _ready() -> void:
 	GUIDE.enable_mapping_context(walk_mode)
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
--- dark-rail-godot/Scenes/player.gd
+++ dark-rail-godot/Scenes/player.gd
@@ -3,17 +3,16 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-
+@onready var camera = $player_camera
+@export var free_mouse:GUIDEAction
 @export var move:GUIDEAction
+@export var look:GUIDEAction
 
 
 func _physics_process(delta: float) -> void:
 	velocity = basis * move.value_axis_3d * SPEED
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	rotation_degrees.y += look.value_axis_1d
+	
+	
+	
 	move_and_slide()
