eec72f229045b8e820629bf33fbdf811c59a2b1a
Earthan Huq <earthan@gmail.com>
2025-05-02 15:33:57 -0400

    Removed a bunch of stuff that had nothing to do with my game.
    These were examples of use-cases and approaches for the GUIDE input-mapping alternative system I installed to help simplify mouse-mapping and capturing.

--- dark-rail-godot/Scripts/track_switch_script.gd
+++ dark-rail-godot/Scripts/track_switch_script.gd
@@ -1,23 +1,5 @@
 extends Area3D
+class_name junction
 
-
-
-# This switch-point, the executor, will reparent the train onto the appropriate
-# track, according to which preswitch was tripped beforehand.
-
-# Whether this switch-point is "looking" the switch-state or ignoring it.
-# Altered by tripping one of three pre-switches.
-# If approaching the junction as a merge, this should be false.
-@onready var switch_look:bool = false
-@onready var train_mover: PathFollow3D = %train_mover
-
-
-func _on_body_entered(body: Node3D) -> void:
-
-	if switch_look:
-		pass
-		# "switch" (reparent) track onto track determined by switch_toggle state
-		
-	else:
-		# merge (reparent) train to track A
-		pass
+@export var Track_L:Path3D
+@export var Track_R:Path3D
